1a38d0ee88e7edd7d70062bdb48db9b1d71acad9
UBSITConsolacionRenato <20217141@s.ubaguio.edu>
2023-03-06 12:40:13 +0800

    added template and styles through TypeScript @Component (March 06)

--- mid-proj/src/app/app.component.ts
+++ mid-proj/src/app/app.component.ts
@@ -1,9 +1,24 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //single-quote ('')
+  //template and style should be enclosed with backtick(``)
+  template: `
+    <h1>Students</h1>
+    <div>
+      <ul class="list-group">
+        <li class="list-group-item" *ngFor="let student of students">
+        {{student.value}}
+        </li>  
+      </ul>
+    </div>`,
+  //adding of styles for the template
+  styles: ['div {background-color: blue; max-width: 250px; margin: 20px;}',
+          'h1 {padding: 10px; margin: 20px;}',
+          'li {font-weight: bold; font-style: italic; max-width: 300px; text-align: center;}',
+          'ul {padding: 30px;}']
+   
 })
 
 export class AppComponent {
@@ -18,6 +33,8 @@ export class AppComponent {
               {value:'Vince Ent'}];
   student = 'Elizabeth';
 }
+
+//interface
 interface Student{
   idNumber: string;
   name: string;
