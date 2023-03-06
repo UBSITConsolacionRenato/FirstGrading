002f39a4212460c7eafbda2e5ec9c7d00ed36c53
UBSITConsolacionRenato <20217141@s.ubaguio.edu>
2023-03-06 13:33:15 +0800

    added template and styles through TypeScript @component (March 06)

--- mid-proj/src/app/app.component.ts
+++ mid-proj/src/app/app.component.ts
@@ -6,15 +6,20 @@ import { Component } from '@angular/core';
   //template and style should be enclosed with backtick(``)
   template: `
     <h1>Students</h1>
-    <div>
+    <div class="stud">
       <ul class="list-group">
         <li class="list-group-item" *ngFor="let student of students">
         {{student.value}}
         </li>  
       </ul>
+    </div>
+    <br><br>
+    <div class='expressions'>
+      <h3> Expenditure = {{item1 + item2 + item3}}</h3>
+      <h3 *ngFor="let item of Items">{{item.value+item.value+item.value}}</h3> 
     </div>`,
   //adding of styles for the template
-  styles: ['div {background-color: blue; max-width: 250px; margin: 20px;}',
+  styles: ['div.stud {background-color: blue; max-width: 250px; margin: 20px;}',
           'h1 {padding: 10px; margin: 20px;}',
           'li {font-weight: bold; font-style: italic; max-width: 300px; text-align: center;}',
           'ul {padding: 30px;}']
@@ -32,6 +37,12 @@ export class AppComponent {
               {value:'Arla Che'},
               {value:'Vince Ent'}];
   student = 'Elizabeth';
+  Items = [{value: 100},
+           {value: 200},
+           {value: 300}];
+  item1 = 100;
+  item2 = 200;
+  item3 = 300;
 }
 
 //interface
